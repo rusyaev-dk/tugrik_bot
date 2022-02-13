94bdc2e219eca510ac15254de8e5237b908b87f7
Dnrvv <unusualman25@gmail.com>
2022-02-13 15:50:15 +0500

    #3 Исправление в set_bot_commands (убрал лишнине команды)

--- tgbot/services/set_bot_commands.py
+++ tgbot/services/set_bot_commands.py
@@ -5,7 +5,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Помощь ()"),
-            types.BotCommand("items", "Айтемы")
+            types.BotCommand("help", "Помощь"),
         ]
     )
